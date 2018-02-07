6e9fa24da5a50211c9fe230cc2ba9ca91ab17245
tarokonno <tarokonno7@gmail.com>
2018-02-07 18:28:39 +1100

    unregister service worker

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render((
   <BrowserRouter>
     <App />
   </BrowserRouter>
 ), document.getElementById('root'));
-// registerServiceWorker();
+
+unregister();
