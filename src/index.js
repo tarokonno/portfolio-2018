14bd65aafbe9382a5db51e67924e35b51b4d0676
tarokonno <tarokonno7@gmail.com>
2018-02-17 14:39:05 +1100

    add scroll to top on route change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 import App from './App';
 import { unregister } from './registerServiceWorker';
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+  	<ScrollToTop>
+    	<App />
+    </ScrollToTop>
   </BrowserRouter>
 ), document.getElementById('root'));
 
